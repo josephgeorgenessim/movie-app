3ed7cc2ef54be0072a5d2dc82d3eed56f75b8240
JosephNessim <josephgeorgenessim@gmail.com>
2025-09-22 14:53:37 +0300

    add dummy screen in saved

--- app/(tabs)/saved.tsx
+++ app/(tabs)/saved.tsx
@@ -1,14 +1,23 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, Image, ScrollView } from 'react-native'
 import React from 'react'
+import { images } from '@/constants/images'
+import { icons } from '@/constants/icons'
 
 const saved = () => {
   return (
-    <View>
-      <Text>saved</Text>
+    <View className='flex-1 bg-primary'>
+      <Image source={images.bg} className="absolute w-full z-0 " />
+      <ScrollView className='flex-1'>
+        <Image source={icons.logo} className="w-12 h-10 mt-20 mb-5  mx-auto" />
+        <View className='mt-10 px-[20px]'>
+          <Text className='text-white font-bold text-2xl mt-2'>Saved Movies</Text>
+        </View>
+        <View className='mt-10 items-center justify-center flex-1'>
+          <Text className='text-light-200 font-bold text-2xl mt-2'>No saved movies</Text>
+        </View>
+      </ScrollView>
     </View>
   )
 }
 
 export default saved
-
-const styles = StyleSheet.create({})
